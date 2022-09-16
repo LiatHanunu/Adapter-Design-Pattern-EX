f8cf20aa39e95887436867d3558d462d8e79e900
LiatHanunu <liat.hanunu01@gmail.com>
2022-09-16 11:06:05 +0300

    adding Processor file

--- dist/Processor.js
+++ dist/Processor.js
@@ -1,1 +1,81 @@
 "use strict";
+// Using interface, 
+//Create Processor interface that hsa a name and process method - gets input as string and returns string.
+class StringProcessor {
+    //     Create StringProcessor as a base class for all  string processors.
+    //     StringProcessor implements Processor interface!
+    //     Leave process method as abstract.
+    get name() { return this.constructor.name; }
+}
+class UpCase extends StringProcessor {
+    //      Create UpCase (class, a kind of Processor)
+    //      Which gets input as a string and returns the string in upper case
+    process(str) {
+        return str.toUpperCase();
+    }
+}
+class DownCase extends StringProcessor {
+    //    Create DownCase (a kind of Processor)
+    //    Which gets input as a string and returns the string in lower case
+    process(str) {
+        return str.toLowerCase();
+    }
+}
+class SplitterMark extends StringProcessor {
+    //    Create SplitterMark (a kind of Processor)
+    //    Which gets input as a string and returns the string with ! in between the words
+    process(str) {
+        return str.replaceAll(" ", "!");
+    }
+}
+class Tester {
+    //     Create a Tester class with static method like so:
+    //    process(p:Processor, s:string):void {
+    // 		log("Using Processor " + p.name)
+    // 		log(p.process(s))
+    //    }
+    process(p, s) {
+        console.log("Using Processor " + p.name);
+        console.log(p.process(s));
+    }
+}
+class StringSwapper {
+    //    Create a new class named String Swapper that has a method named swap, taking a string and returns a string.
+    //    This method swaps each 2 characters, starting from both sides of the string, in that input ('Ahalan' -> 'nalahA')
+    swap(str) {
+        const tempArray = str.split("");
+        let j = tempArray.length - 1;
+        for (let i = 0; i < tempArray.length / 2; i++) {
+            let tempCh = tempArray[i];
+            tempArray[i] = tempArray[j];
+            tempArray[j] = tempCh;
+            j--;
+        }
+        return tempArray.join("");
+    }
+}
+class Adapter {
+    constructor(adaptee) {
+        this.adaptee = adaptee;
+    }
+    get name() { return this.adaptee.constructor.name; }
+    process(str) {
+        return this.adaptee.swap(str);
+    }
+}
+//Create processors objects and test them via Tester.process
+const uCase2 = new UpCase();
+const dCase2 = new DownCase();
+const sMark2 = new SplitterMark();
+const test2 = new Tester();
+const sSwap2 = new StringSwapper();
+const adapter2 = new Adapter(sSwap2);
+test2.process(uCase2, "hi"); //Using Processor UpCase HI
+test2.process(dCase2, "HI"); //Using Processor DownCase hi
+test2.process(sMark2, "how are you"); //Using Processor SplitterMark how!are!you
+test2.process(adapter2, "Ahalan"); //Using Processor StringSwapper nalahA
+// * What design pattern did we learn here? 
+// ANSWER:  Adapter pattern.
+// Adapter is a structural design pattern, which allows incompatible objects to collaborate.
+// The Adapter acts as a wrapper between two objects.
+// It catches calls for one object and transforms them to format and interface recognizable by the second object.
